c3fa6432f9ecd8f945bae56caa3bb290e7f167cb
Simon Burdall <Simon@burdall.com>
2023-01-26 23:01:24 +0000

    Addressed issue in Sheets adding rogue quote

--- glManual.py
+++ glManual.py
@@ -1,10 +1,17 @@
 import glSheets, glGiantBomb
 import datetime
 
-date = datetime.datetime.now()
-gameFirstPlayed = date.strftime("%d/%m/%Y")
+
 
 gameTitle = input("Please enter a Game title: ")
+
+gameFirstPlayed = input ("Please enter date First played, format DD/MM/YYYY, if not today: ")
+
+if not gameFirstPlayed:
+    date = datetime.datetime.now()
+    gameFirstPlayed = date.strftime("%d/%m/%Y")
+else:
+    pass
 
 gameTitle, gameDeveloper, gamePublisher, gameGenre, gamePlatform, gameReleaseYear = glGiantBomb.gbCheck(gameTitle)
 glSheets.gameCheck(gameTitle, gameDeveloper, gamePublisher, gameGenre, gamePlatform, gameReleaseYear, gameFirstPlayed)
--- glSheets.py
+++ glSheets.py
@@ -1,6 +1,7 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import config
+import datetime
 
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name(config.sheetsJson, scope)
@@ -11,12 +12,15 @@ def gameCheck(gameTitle, gameDeveloper, gamePublisher, gameGenre, gamePlatform, 
     gameLog = sheet.get_all_records()
     
     foundTitle = False
-
+    
+    totalRows = sheet.row_count
+    gameSinceRelease = f'=DATEDIF(F{totalRows+1},G{totalRows+1},"y")&" years, "&DATEDIF(F{totalRows+1},G{totalRows+1},"ym")&" months, and "&DATEDIF(F{totalRows+1},G{totalRows+1},"md")&" days"'
+    
     for row in gameLog:
         if row['Title'] == gameTitle and row['First Release Date'] == gameReleaseYear:
             foundTitle = True
             print(f'{gameTitle} already exists.')
             break
     if not foundTitle:
-        sheet.append_row([gameTitle, gameDeveloper, gamePublisher, gameGenre, gamePlatform, gameReleaseYear, gameFirstPlayed])
+        sheet.append_row([gameTitle, gameDeveloper, gamePublisher, gameGenre, gamePlatform, gameReleaseYear, gameFirstPlayed, gameSinceRelease],value_input_option='USER_ENTERED')
         print(f'{gameTitle} added.')
