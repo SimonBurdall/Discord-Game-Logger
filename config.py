b5d1e6310d3b08eb4518674bfb41541a8ffcacd5
Simon Burdall <Simon@burdall.com>
2023-01-29 03:20:42 +0000

    Removed and added new varibles for IGDB

--- config.py
+++ config.py
@@ -1,6 +1,28 @@
-discordKey = ""
-togglKey = ""
-giantBombKey = ""
-userID = 
+discord_Key = ""
+discordUser_id =
+toggl_Key = ""
+igdbClient_id = ""
+igbdAuth_token = 
 sheetsJson = ""
 sheetName = ""
+weighted_platforms = {
+    "Android": 1,
+    "Gameboy": 2,
+    "PlayStation": 3,
+    "Gameboy Color": 4,
+    "Dreamcast": 5,
+    "PlayStation 2": 6,
+    "Gameboy Advance": 7,
+    "PC (Microsoft Windows)":8,
+    "Xbox": 10,
+    "DS": 11,
+    "Xbox 360": 13,
+    "PlayStation 3": 11,
+    "Wii": 9,
+    "3DS": 14,
+    "Xbox One": 16,
+    "PlayStation 4": 17,
+    "Switch": 15,
+    "Xbox Series X|S": 18,
+    "PlayStation 5": 19
+}
